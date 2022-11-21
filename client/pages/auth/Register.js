7288cc77715b37ef515c5b9026a425f0bb56c0d4
brian nelson <bn3l10@gmail.com>
2022-11-21 11:18:56 -0500

    finished register

--- client/pages/auth/Register.js
+++ client/pages/auth/Register.js
@@ -4,6 +4,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { registerUser, validateEmail } from "../../services/authService";
 import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const initialState = {
     name: "",
@@ -54,8 +65,88 @@ const Register = () => {
             setIsLoading(false);
         }
     };
+    const theme = createTheme();
 
     return (
-        
-    )
-}
+        <ThemeProvider theme={theme}>
+            <Container comonent="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box 
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex', 
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1"variant="h5">
+                            Sign Up
+                        </Typography>
+                        <Box comonent="form" onSubmit={register} sx={{ mt:3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="name"
+                                        required
+                                        fullWidth
+                                        id="name"
+                                        label="Name"
+                                        autoFocus
+                                        value={name}
+                                        onChange={handleInputChange}
+                                        />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        value={email}
+                                        onChange={handleInputChange}
+                                        />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        id="password"
+                                        label="Password"
+                                        type="password"
+                                        autoComplete="new-password"
+                                        value={password}
+                                        onChange={handleInputChange}
+                                        />
+                                </Grid>
+                            </Grid>
+                            <Button 
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                        >
+                                            Sign Up
+                                        </Button>
+                                        <Grid container justifyContent="flex-end">
+                                            <Grid item>
+                                                <Link to="/" variant="body2">Home</Link>
+                                                <Link to="/login" variant="body2">
+                                                    Already have an account? Sign in
+                                                </Link>
+                                            </Grid>
+                                        </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default Register;
